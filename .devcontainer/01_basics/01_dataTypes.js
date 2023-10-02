24b54f60f8817b716d848fb9803068398afb1c45
Somanath Gaikwad <103896679+somanathg@users.noreply.github.com>
2023-10-02 19:12:36 +0000

    variables

---
+++ .devcontainer/01_basics/01_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treated as New JS version
+
+//alert(3+3);//we are using node js not browser
+console.log(3+4);
+
+//datatypes
+//number
+//bigint
+//string
+
+//boolean
+//null=>standalone value--object
+//undefined=>not defined--type
+//symbol=>unique
+
+//object
---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=131;
+let accountEmail="test@gmail.com";
+var accountPassword="12345";
+accountCity="jaipur";
+//accountId=123;//const change not allowded
+console.log(accountId);
+accountEmail="asas@gmail.com";
+
+console.table([accountId,accountEmail,accountPassword]);
+// dont use var ,use const ,let
